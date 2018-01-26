940c2d5fe7cdbfeb9623a16fbf81d36371a1f494
Amie Chen <chinghc@gmail.com>
2018-01-25 23:14:24 -0800

    render rgba from hex

--- drib.js
+++ drib.js
@@ -1,4 +1,3 @@
-document.body.style.border = "5px solid red";
 var colorChips = document.querySelectorAll('.color-chips > .color a');
 var paletteJson = {
   "compatibleVersion": "1.4",
@@ -6,9 +5,23 @@ var paletteJson = {
   "colors": []
 };
 
+function hexToRGBA(hex) {
+   hex = hex.replace('#', '');
+   var r = parseInt(hex.length == 3 ? hex.slice(0, 1).repeat(2) : hex.slice(0, 2), 16);
+   var g = parseInt(hex.length == 3 ? hex.slice(1, 2).repeat(2) : hex.slice(2, 4), 16);
+   var b = parseInt(hex.length == 3 ? hex.slice(2, 3).repeat(2) : hex.slice(4, 6), 16);
+
+   return {
+    "red": r,
+    "green": g,
+    "blue": b,
+    "alpha": 1
+   };
+}
+
 for (var i=0; i < colorChips.length; i++) {
   var hex = colorChips[i].getAttribute('title');
-  paletteJson.colors.push(hex);
+  paletteJson.colors.push(hexToRGBA(hex));
 }
 
 console.log(paletteJson);
